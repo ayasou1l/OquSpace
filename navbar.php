bffeb7c95745f6a1bcedcddeff4b160bd06094d5
ayasou1l <armanova.aya123@gmail.com>
2026-04-19 16:46:34 +0500

    navbar.php

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+<form class="global-search" method="GET" action="search_users.php">
+
+    <input type="text" name="q" placeholder="🔍 Найти пользователя...">
+
+</form>
+
+<div class="logo">📚 OquSpace</div>
+
+<div class="nav-links">
+    <a href="index.php">🏠 Лента</a>
+    <a href="groups.php">👥 Группы</a>
+    <a href="materials.php">📚 Учебные материалы</a>
+    <a href="video_lessons.php">🎥 Видео уроки</a>
+    <a href="profile.php">👤 Профиль</a>
+</div>
+
+<div class="nav-right">
+    <a href="search_users.php" class="nav-icon">🔍</a>
+    <a href="logout.php" class="logout-link">🚪 Выйти</a>
+</div>
+</div>
+
+</nav>
