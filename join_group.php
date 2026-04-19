8c8518f01547983c78d361a6eab238f680e00363
ayasou1l <armanova.aya123@gmail.com>
2026-04-19 16:43:42 +0500

    join_group.php

---
+++ join_group.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include 'db.php';
+
+$user_id = $_SESSION['user_id'];
+$group_id = $_GET['group_id'];
+
+// проверка (чтобы не вступал 100 раз)
+$check = $conn->query("
+    SELECT * FROM group_members 
+    WHERE user_id=$user_id AND group_id=$group_id
+");
+
+if ($check->num_rows == 0) {
+    $conn->query("
+        INSERT INTO group_members (user_id, group_id)
+        VALUES ($user_id, $group_id)
+    ");
+}
+
+header("Location: group.php?id=$group_id");
+exit;
+?>
